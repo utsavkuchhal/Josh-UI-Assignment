dda39794141c686d0e7a174692bac27b4e59dd0b
= <=>
2021-09-20 18:30:20 +0530

    files added

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items: 3,
+    navText: ["<img src='assets/images/arrow-left.png'>","<img src='assets/images/arrow-right.png'>"]
+  });
+});
